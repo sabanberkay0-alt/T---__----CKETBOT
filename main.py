aa603eb8c194a45a5fb96eebaff00a1fa32d1797
sabanberkay0-alt <sabanberkay0@gmail.com>
2026-02-21 12:57:31 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} aktif!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! 🏓")
+
+bot.run(TOKEN)
